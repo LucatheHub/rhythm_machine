8fbe2e11fead2782fa13918f2dbe7764ad4731fd
LucaTheHub <lucascorleto@gmail.com>
2023-12-13 21:24:48 +0100

    avances

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ let sequenceInterval;
 let sequenceCounter = 1;
 const audioMetro = new Audio('sonidos/metro.mp3');
 const audioSnare = new Audio('sonidos/kick.mp3');
-
+let audioNuevo;
 
 document.addEventListener('DOMContentLoaded', () => {
   const playAudio = (audio) => {
@@ -34,6 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
       // Reproduce el sonido de la snare al mismo tiempo
       playAudio(audioSnare);
 
+      // Reproduce el nuevo sonido si está definido
+      if (audioNuevo) {
+        playAudio(audioNuevo);
+      }
+
       socket.emit('changeButtonColor', sequenceCounter);
       sequenceCounter = (sequenceCounter % 16) + 1;
     }, intervaloSlider);
@@ -55,6 +60,12 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Secuencia pausada');
     clearInterval(sequenceInterval);
     sequenceCounter = 1;
+
+    // Detener el nuevo audio si está definido
+    if (audioNuevo) {
+      audioNuevo.pause();
+      audioNuevo.currentTime = 0;
+    }
   };
 
   const actualizarIntervalo = (nuevoIntervalo) => {
@@ -88,4 +99,52 @@ document.addEventListener('DOMContentLoaded', () => {
     detenerSecuencia();
     socket.emit('pauseSequence');
   });
+
+  const dropArea = document.getElementById('drop-area');
+  const fileInput = document.getElementById('file-input');
+
+  // Manejar el evento de soltar en el área
+  dropArea.addEventListener('drop', handleDrop);
+
+  // Evitar el comportamiento predeterminado para el área de soltar
+  dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  // Manejar el evento de clic en el área para abrir el selector de archivos
+  dropArea.addEventListener('click', () => {
+    fileInput.click();
+  });
+
+  // Manejar el evento de cambio en el input de archivos
+  fileInput.addEventListener('change', (event) => {
+    handleFiles(event.target.files);
+  });
+
+  function handleFiles(files) {
+    if (files.length > 0) {
+      const file = files[0];
+      loadAudio(file);
+    } else {
+      console.error('No se ha seleccionado ningún archivo.');
+    }
+  }
+
+  // Función para manejar archivos soltados en el área
+  function handleDrop(e) {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    handleFiles(files);
+  }
+
+  function loadAudio(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const audioUrl = e.target.result;
+      audioNuevo = new Audio(audioUrl);
+    };
+
+    reader.readAsDataURL(file);
+  }
 });
